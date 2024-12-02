d3bad53c2a7ca0820426fb7482643be09ccb2de4
Kartik Vashistha <kartikv.work@gmail.com>
2024-12-02 14:06:03 +0000

    feat: update rust files

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -3,6 +3,10 @@ fn main() {
 
     let mut c_config = cc::Build::new();
     c_config.std("c11").include(src_dir);
+    c_config
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .flag_if_supported("-Wno-unused-value")
+        .flag_if_supported("-Wno-implicit-fallthrough");
 
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
@@ -12,11 +16,10 @@ fn main() {
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     // NOTE: if your language uses an external scanner, uncomment this block:
-    /*
+
     let scanner_path = src_dir.join("scanner.c");
     c_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    */
 
     c_config.compile("tree-sitter-ansible");
 }
